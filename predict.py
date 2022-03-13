6aa0fefbaf2fd38390efd71cf5dd2756993239ef
Deepali <deep.n.lp@gmail.com>
2022-03-13 14:34:12 -0400

    Second commit

--- predict.py
+++ predict.py
@@ -13,5 +13,5 @@ def ordinal_encoder(input_val, feats):
 
 def get_prediction(data,model):
     target=['Slight Injury','Serious Injury','Fatal injury']
-    res = target[model.predict(data)]
+    res = target[(model.predict(data)).astype('int64')]
     return model.predict(data)
